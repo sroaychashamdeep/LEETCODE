91737addcf37e78646d87d4cc3f05da566d551ca
Chashamdeep Sroay <sroaychashamdeep@gmail.com>
2026-09-06 19:17:46 +0530

    Time: 11 ms (87.61%) | Memory: 8.9 MB (95.82%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+
+        const long long LIMIT = INT_MAX;
+
+        vector<long long> dp(m + 1, 0);
+
+        // Empty t can always be formed in 1 way
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j >= 1; j--) {
+
+                if (s[i - 1] == t[j - 1]) {
+                    dp[j] += dp[j - 1];
+
+                    // Prevent overflow
+                    if (dp[j] > LIMIT) {
+                        dp[j] = LIMIT;
+                    }
+                }
+            }
+        }
+
+        return (int)dp[m];
+    }
+};
